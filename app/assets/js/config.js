ed7ee9bafe0f2ba72d8e252c13b32140025142fa
cnoss <christian.noss@fh-koeln.de>
2016-10-29 20:15:39 +0200

    Basisversion erstellt

---
+++ app/assets/js/config.js
@@ -0,0 +1,5 @@
+var base = {};
+base.templates = "assets/templates";
+
+var templateUrls = {};
+templateUrls.quizOverview = base.templates + "/quiz-overview.htm";
---
+++ app/assets/js/view.js
@@ -0,0 +1,40 @@
+function createQuizOverview(){
+	
+	getTemplate(templateUrls.quizOverview).then(function(response) {
+    	// The first runs when the promise resolves, with the request.reponse
+		// specified within the resolve() method.
+				
+		var target = document.getElementById("content");
+		target.innerHTML = response;
+		
+		// Item Template ziehen
+		var item = document.getElementById("quiz-item");
+		//document.removeChild(item);
+		console.log(item);
+		
+
+    // The second runs when the promise
+    // is rejected, and logs the Error specified with the reject() method.
+  	}, function(Error) {
+    	console.log(Error);
+  	});
+	
+	// Lies das Wrap Templates aus und gib mir den Inhalt
+	//var wrapTemplate = getTemplate(templateUrls.quizOverviewWrap);
+	//alert(wrapTemplate);
+	
+	
+	// Gib mir die URL des Item Templates
+	//var itemUrl = getTemplateUrl(templateUrls.quizOverviewItem);
+	
+	// Lies es aus und gib mir den Inhalt
+	//var itemTemplate = getTemplate(itemUrl);
+	
+	// Injiziere die Items in den Wrap
+	
+	
+	// Injiziere den Content in Quiz Wrap
+	
+	
+	
+}
